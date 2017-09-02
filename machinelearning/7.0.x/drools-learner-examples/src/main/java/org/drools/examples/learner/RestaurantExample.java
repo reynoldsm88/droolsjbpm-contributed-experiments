21012b641f7cd827fbe68f5fe8aa7d3c1087940b
Michael Reynolds <reynoldsm88@gmail.com>
2017-09-02 13:21:36 -0400

    adding poker and restauraunt examples with subset of data that appears to break the learner

---
+++ machinelearning/7.0.x/drools-learner-examples/src/main/java/org/drools/examples/learner/RestaurantExample.java
@@ -0,0 +1,47 @@
+package org.drools.examples.learner;
+
+import java.util.List;
+
+import org.drools.examples.learner.models.Car;
+import org.drools.examples.learner.models.Restaurant;
+import org.drools.examples.learner.utils.CsvFileReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RestaurantExample {
+
+    private static final Logger LOG = LoggerFactory.getLogger( RestaurantExample.class );
+    private static final boolean PRINT_DRL = true;
+
+    public static void main( String... args ) throws Exception {
+
+        String inputFile = System.getProperty( "user.dir" ) + "/src/main/resources/data/restauraunt/restauraunt.data.csv";
+        LOG.info( "parsing training data file " + inputFile );
+        List<Object> restaurantData = CsvFileReader.readObjects( inputFile, csv -> {
+            Restaurant restauraunt = new Restaurant();
+            restauraunt.setAlternate( Boolean.parseBoolean( csv.get( "alternate" ).trim() ) );
+            restauraunt.setBar( Boolean.parseBoolean( csv.get( "bar" ).trim() ) );
+            restauraunt.setFriSat( Boolean.parseBoolean( csv.get( "fri_sat" ).trim() ) );
+            restauraunt.setHungry( Boolean.parseBoolean( csv.get( "hungry" ).trim() ) );
+            restauraunt.setPatrons( csv.get( "patrons" ) );
+            restauraunt.setPrice( Integer.parseInt( csv.get( "price" ).trim() ) );
+            restauraunt.setRaining( Boolean.parseBoolean( csv.get( "raining" ) ) );
+            restauraunt.setReservation( Boolean.parseBoolean( csv.get( "reservation" ).trim() ) );
+            restauraunt.setType( csv.get( "type" ).trim() );
+            restauraunt.setWaitEstimate( csv.get( "wait_estimate" ).trim() );
+            restauraunt.setWillWait( Boolean.parseBoolean( csv.get( "will_wait" ).trim() ) );
+            return restauraunt;
+        } );
+
+        LOG.info( "beginning training example" );
+        DroolsLearnerExample carExample = new DroolsLearnerExample( DroolsLearnerExample.SINGLE_C45E );
+        carExample.runTraningExample( Restaurant.class, restaurantData );
+
+        if ( PRINT_DRL ) {
+            System.out.println( carExample.getDrl() );
+        }
+        LOG.info( "executing test data set against genearted rules" );
+        carExample.runGeneratedRules( restaurantData );
+    }
+
+}
